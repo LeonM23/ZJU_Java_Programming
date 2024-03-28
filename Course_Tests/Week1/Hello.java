954c80cd9edaa07060040bf717e527e03796714c
qzmei <qzmei1997@163.com>
2024-03-28 19:19:31 +0800

    rename

---
+++ Course_Tests/Week1/Hello.java
@@ -0,0 +1,14 @@
+package Course_Tests.Week1;
+
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextLine());
+        in.close();
+    }
+     
+}
