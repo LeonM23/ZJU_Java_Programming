a73ad653e60ff9a30aa31f69c6b74bf0899c11df
qzmei <qzmei1997@163.com>
2024-03-28 23:03:34 +0800

    rename

--- Pintia_Problem_Set/7-1 重要的话说三遍/Main.java
+++ Pintia_Problem_Set/7-1 重要的话说三遍/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");        
+        System.out.println("I'm gonna WIN!");        
+        System.out.println("I'm gonna WIN!");        
+        System.out.println("I'm gonna WIN!");        
     }
 }
--- Pintia_Problem_Set/7-6 厘米换算英尺英寸/Main.java
+++ Pintia_Problem_Set/7-6 厘米换算英尺英寸/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");        
+        Scanner in = new Scanner(System.in);
+        int height = in.nextInt();
+
+        height = (int) (height * 12 / 30.48);
+        System.out.println(height / 12 + " " + height % 12);
     }
+
 }
